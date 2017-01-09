b9bb08494d300139c8fe3aa1b4b370a46c8cc899
Gibral Raphael & Nadav Appel & Patrick Kolodgy <pair+graphael7.nadavp3.pkolodgy@devbootcamp.com>
2017-01-09 12:39:10 -0500

    Update validations

--- app/models/owned_stock.rb
+++ app/models/owned_stock.rb
@@ -1,4 +1,9 @@
 class OwnedStock < ApplicationRecord
   belongs_to :user
   belongs_to :stock
+
+  validates :shares, presence: true
+  validates :shares, numericality: {only_integer: true}
+  validates :original_price, presence: true
+  validates :original_price, numericality: {only_float: true}
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
   has_many :positions, class_name: OwnedStock
   has_many :owners, through: :positions, source: :user
+
+  validates :ticker, presence: true
+  validates :ticker, uniqueness: true
+  
 end
