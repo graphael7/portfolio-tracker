cae6957d24cb20f45c63773f6b75d916879e68da
Patrick Kolodgy <email": null>
2017-01-09 19:35:54 -0500

    Create method to return user's portfolio history

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,19 +39,37 @@ class User < ApplicationRecord
     portfolio[:portfolio_pnl] = portfolio_pnl.round(4)
     portfolio[:portfolio_cost_basis] = portfolio_cost_basis.round(4)
     portfolio[:portfolio_total_value] = portfolio_total_value.round(4)
-    portfolio[:pnl_history] = generate_pnl_history
+    portfolio[:history] = generate_portfolio_history
     portfolio
   end
 
-  def generate_pnl_history
+  def generate_portfolio_history
     pnl_history = []
     number_of_days = (Date.today - self.owned_stocks.first.created_at.to_date).to_i
     historical_portfolio = []
-    while number_of_days > -1
-      buys = OwnedStock.where("DATE(created_at) < ?", Date.today - number_of_days)
-      
+    while number_of_days > 0
+      date = Date.today - number_of_days
+      if trading_day?(date)
+        daily = {}
+        daily[:date] = date
+        daily[:portfolio] = []
+        daily[:overall_value] = 0
+        daily[:total_pnl] = 0
+        buys = OwnedStock.where("DATE(created_at) < ?", date)
+        buys.each do |buy|
+          daily[:portfolio] << {ticker: buy.stock.ticker, shares: buy.shares, original_price: buy.original_price}
+          stock_historical_value = StockQuote::Stock.history(buy.stock.ticker, date, date).first.adj_close
+          daily[:overall_value] += (stock_historical_value * buy.shares)
+          daily[:total_pnl] += (daily[:overall_value] - (buy.shares * buy.original_price))
+          historical_portfolio << daily
+        end
+      end
+      number_of_days -= 1
     end
     historical_portfolio
   end
 
+  def trading_day?(date)
+    StockQuote::Stock.history("FB",date,date).class == Array
+  end
 end
